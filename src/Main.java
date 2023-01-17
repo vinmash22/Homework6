f52f0a9a6ae84243e3d1c9f8ad9ee26048447d66
vinmash <vinmash@mail.ru>
2023-01-17 20:45:49 +0500

    task 1-7

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
+        task6();
+        task7();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -26,6 +29,28 @@ public class Main {
     public static void task4() {
         System.out.println("Задача 4");
         for (int i = 10; i >= -10; i = i-1) {
+            System.out.println(i);
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i+4) {
+            int proverka1=i%100;
+            int proverka2=i%400;
+            if (proverka1 != 0 || proverka2 == 0) {
+                System.out.println("Високосный год " + i);
+            }
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i+7) {
+                            System.out.println(i);
+            }
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i*2) {
             System.out.println(i);
         }
     }
